c4403d6a5fd558fcb7998be722209bd1da18888f
gshveta <gshveta@seas.upenn.edu>
2017-07-31 15:41:07 -0400

    Add files via upload

---
+++ Code_version1_0731.py
@@ -0,0 +1,46 @@
+import time
+import RPi.GPIO as GPIO
+import os # to set the timezone
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(3, GPIO.OUT)
+
+FlagStartCounter = 0
+os.environ['TZ'] = 'US/Eastern'#'Africa/Kigali'
+time.tzset()
+''' for reference
+year = t[0]
+day = t[1]
+month = t[2]
+hour = t[3]
+minutes = t[4]
+seconds = t[5]
+'''
+
+while True:
+	t = time.localtime()
+	if (t[3] == 7 and t[4] == 35 and t[5] == 0) is True: # start time for 7.35am
+		GPIO.output(3, True) # ring the bell at start
+		print (t)
+		hour = t[3]
+		minutes = t[4]
+		seconds = t[5]
+		FlagStartCounter = True
+		break
+	else:
+		FlagStartCounter = False
+
+while FlagStartCounter is True:
+	time.sleep(600) # check every 10 minutes = 600 seconds
+	t = time.localtime()
+	if (t[3] - hour is 1) and (t[4] - minutes <=2):
+		GPIO.output(3, True) # activate alarm every 60 minutes
+		minutes = t[4]
+		hour = t[3]
+		t = time.localtime()
+		print (t)
+	else:
+		GPIO.output(3, False)
+	
+		
